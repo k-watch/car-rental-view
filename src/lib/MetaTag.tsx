f80c9be48e0c27272655868d943067dfa3e857c2
kyh <kwonyong6@gmail.com>
2022-11-04 00:06:26 +0900

    🐛[fix] 메타태그 작업
    description 변경

--- src/lib/MetaTag.tsx
+++ src/lib/MetaTag.tsx
@@ -1,6 +1,5 @@
 import { Helmet } from 'react-helmet-async';
 import { CarInterface } from 'types/api';
-import { SEGMENT } from 'types/enum';
 import { getAmountPattern } from './common';
 
 const MetaTag = (props: CarInterface) => {
@@ -9,15 +8,16 @@ const MetaTag = (props: CarInterface) => {
     <Helmet>
       <title>{`${attribute.brand} ${attribute.name}`}</title>
 
-      <meta name="description" content={`${getAmountPattern(amount)} 원`} />
-
       <meta property="og:type" content="website" />
       <meta
         property="og:title"
         content={`${attribute.brand} ${attribute.name}`}
       />
       <meta property="og:site_name" content="차량대여 서비스" />
-      <meta property="og:description" content={SEGMENT[attribute.segment]} />
+      <meta
+        property="og:description"
+        content={`${getAmountPattern(amount)} 원`}
+      />
       <meta property="og:image" content={attribute.imageUrl} />
     </Helmet>
   );
